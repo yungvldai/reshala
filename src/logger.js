286b843c2c529b9bc9e7a9011c1f953e00406e31
Vlad Ivanov <vladislav.ivanov@onetwotrip.com>
2022-02-11 14:16:25 +0300

    small fixes

--- src/logger.js
+++ src/logger.js
@@ -7,13 +7,13 @@ const debug = (...args) => {
     return;
   }
 
-  console.log(app, `[${chalk.magenta('debug')}]:`, ...args);
+  console.log(app, chalk.magenta('DEBUG'), ...args);
 };
 
 const info = (...args) => console.log(app, ...args);
 
-const warn = (...args) => console.log(app, chalk.yellow('WARN!'), ...args);
+const warn = (...args) => console.log(app, chalk.yellow('WARN'), ...args);
 
-const err = (...args) => console.log(app, chalk.red('ERR!'), ...args);
+const err = (...args) => console.log(app, chalk.red('ERROR'), ...args);
 
 export default { debug, info, err, warn };
